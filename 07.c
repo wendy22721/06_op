cb2e48acbaff491b488ea9a02815aa6a142da0b2
wendy22721 <55476967+wendy22721@users.noreply.github.com>
2019-10-15 18:53:40 +0900

    07

---
+++ 07.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int factorial(int n)
+{
+	int res=1; 
+	int i;
+	for(i=1;i<=n;i++)
+	{
+		res = res *i;
+	}
+	return res;
+}
+
+int calcCombination(int n, int r)
+{
+	int high, low;
+	high = factorial(n);
+	low = factorial(n-r)*factorial(r);
+	
+	return (high/low);
+}
+
+int main(int argc, char *argv[]) 
+{
+	int n, r;
+	int res;
+	
+	printf("input n and r");
+	scanf("%d %d", &n, &r);
+	
+	res = calcCombination(n, r);
+	printf("combination result is %d\n", res);
+
+	return 0;
+	
+	
+}
